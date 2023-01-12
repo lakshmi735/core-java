384f2969b4b45be41753331b8f09f59df3486367
lakshmi735 <lakshmim.xworks@gmail.com>
2023-01-12 18:21:18 +0530

    uploading test1

---
+++ ReverseStrings.java
@@ -0,0 +1,13 @@
+class ReverseStrings 
+{
+	public static void main(String a[])
+	{
+		String name="program";
+		char c[]=name.toCharArray();
+		for(int i=c.length-1;i>=0;i--)
+		{
+			
+		System.out.println(c[i]);
+		}
+		
+	}
